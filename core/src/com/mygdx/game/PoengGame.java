a0ad28000a3dac61b19492a1b4f3b7e4dd29396f
Jon Terner <jon.programming@gmail.com>
2017-03-01 19:14:34 -0500

    Removed bad android image

--- core/src/com/mygdx/game/PoengGame.java
+++ core/src/com/mygdx/game/PoengGame.java
@@ -13,7 +13,7 @@ public class PoengGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		System.out.println("Hello World\n");
 	}
 
 	@Override
@@ -21,7 +21,6 @@ public class PoengGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
