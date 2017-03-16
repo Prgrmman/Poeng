59f8328b975f90d8156de8c960908e693e3b6924
Jon Terner <jon.programming@gmail.com>
2017-03-15 22:08:26 -0500

    Fixed desktop working directory and added music and display

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.mygdx.game.PoengGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Poeng";
+		config.useGL30 = true;
+		config.width = 1500;
+		config.height = 750;
 		new LwjglApplication(new PoengGame(), config);
 	}
 }
