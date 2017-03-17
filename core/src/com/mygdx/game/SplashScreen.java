79537172742afa76d165dfcba34e1c693d67f5dc
Jon Terner <jon.programming@gmail.com>
2017-03-16 19:44:28 -0500

    tried to fix build files

--- core/src/com/mygdx/game/SplashScreen.java
+++ core/src/com/mygdx/game/SplashScreen.java
@@ -2,11 +2,19 @@ package com.mygdx.game;
 
 /*
  * This class will serve to represent the loading screen in the beginning
- * It will have support for buttons, which should be clickable or tapable (android)
- * 
- * There will also be menu options here as well
- * Once the splashscreen is no longer needed, we will close it and get a series of responses that will determine the game mode
+ * This will just have an opening animation
  */
 public class SplashScreen {
 // TODO: implement menu stuff
+	
+	
+	
+	public SplashScreen(){
+		
+	}
+	
+	
+	public void run(){
+		// runs the splash screen animation
+	}
 }
