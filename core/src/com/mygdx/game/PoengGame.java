95ce019538bfc882ad4184e149cc90b419478482
Jon Terner <jon.programming@gmail.com>
2017-03-16 23:41:56 -0500

    Added splash screen

--- core/src/com/mygdx/game/PoengGame.java
+++ core/src/com/mygdx/game/PoengGame.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
@@ -8,53 +9,37 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class PoengGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
-	private Music bgMusic;
-	private OrthographicCamera camera;
+public class PoengGame extends Game {
+	SpriteBatch batch;
+	Music bgMusic;
 	
 	// This is a test of the ball
-	private Ball ball;
 
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		bgMusic = Gdx.audio.newMusic(Gdx.files.internal("data/bgm.ogg"));
-		img = new Texture(Gdx.files.internal("data/cat.gif"));
-		ball = new Ball(0,0);
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1600, 900);
+
 		bgMusic.setLooping(true);
 		bgMusic.play();
+		
+		this.setScreen((new SplashScreen(this)));
 
 	}
 
 	@Override
 	public void render () {
-		// Draw Backgound
-		Gdx.gl.glClearColor(210/255f, 210/255f, 210/255f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		// run game loop
-		loop();
-		// Draw sprite batch
-		batch.begin();
-		ball.draw(batch);
-		batch.end();
-		camera.update();
+		super.render();
 	}
 	
 	// game loop
 	public void loop(){
-		ball.moveRight();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 		bgMusic.dispose();
 	}
 }
--- core/src/com/mygdx/game/SplashScreen.java
+++ core/src/com/mygdx/game/SplashScreen.java
@@ -1,20 +1,92 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /*
  * This class will serve to represent the loading screen in the beginning
  * This will just have an opening animation
  */
-public class SplashScreen {
+public class SplashScreen implements Screen {
 // TODO: implement menu stuff
+	final PoengGame game; // refers to the game instance
+	Texture logo;
+	SpriteBatch batch;
+	OrthographicCamera camera;
+	int screenWidth;
+	int screenHeight;
 	
 	
-	
-	public SplashScreen(){
-		
+	public SplashScreen(final PoengGame game){
+		this.game = game;
+		logo = new Texture(Gdx.files.internal("data/logo.gif"));
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, screenWidth, screenHeight);
+		System.out.println(logo.getWidth());
+		System.out.println(Gdx.graphics.getWidth());
 	}
 	
-	
-	public void run(){
-		// runs the splash screen animation
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(210/255f, 210/255f, 210/255f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		// game.batch.setProjectionMatrix(camera.combined);
+		game.batch.enableBlending();
+		game.batch.begin();
+		game.batch.draw(logo, 0, 0);
+		game.batch.end();
+		
+		// when finished with splash
+		// game.setScreen(something here)
+		
+	}
+
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
 	}
 }
